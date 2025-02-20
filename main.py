1e854cd2941f309db6711d8e213e3721cb8c6575
Ilya <ilyayakub67@gmail.com>
2025-02-20 16:44:17 +0300

    movement along x-axis

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class Game:
         while True:
             self.current_window.fill((14, 219, 248))
 
+            self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.current_window)
 
             for event in pygame.event.get():
@@ -36,16 +37,16 @@ class Game:
                     sys.exit()
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
 
             self.window.blit(pygame.transform.scale(self.current_window, self.window.get_size()), (0, 0))
             pygame.display.update()
